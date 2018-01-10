3bbc5b6b4da3022e1566dd10ab960b1ce9e2bb5a
Ryan Flach <ryanflach@users.noreply.github.com>
2018-01-10 14:47:43 -0700

    Tuple assignment / retrieval type A

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
+    let tup = (500, 6.4, 1);
+
+    let (_x, y, _z) = tup;
+
+    println!("The value of y is: {}", y);
 }
