3d42b276c2c49b675fa459df54cb561a16bc2b5e
Ryan Flach <ryanflach@users.noreply.github.com>
2018-01-10 16:51:31 -0700

    Loops

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,12 +1,27 @@
 fn main() {
-    let condition = true;
+    // Examples of the three loop types in Rust.
 
-    // types in the conditional must be the same when using for variable assignment
-    let number = if condition {
-        5
-    } else {
-        6
-    };
+    loop {
+        println!("again!");
+        // Would otherwise run until manually aborted
+        break;
+    }
 
-    println!("The value of number is: {}", number);
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number = number - 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("The value is: {}", element);
+    }
 }
