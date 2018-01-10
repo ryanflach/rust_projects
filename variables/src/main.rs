38d1c29b7c7da1d8fda7f934655e14579ff63e1f
Ryan Flach <ryanflach@users.noreply.github.com>
2018-01-10 14:49:40 -0700

    Tuple assignment / retrieval type B

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,13 @@
 fn main() {
-    let tup = (500, 6.4, 1);
+    let x: (i32, f64, u8) = (500, 6.4, 1);
 
-    let (_x, y, _z) = tup;
+    let five_hundred = x.0;
 
-    println!("The value of y is: {}", y);
+    let six_point_four = x.1;
+
+    let one = x.2;
+
+    println!("The value of the first element is {}", five_hundred);
+    println!("The value of the second element is {}", six_point_four);
+    println!("The value of the third element is {}", one);
 }
