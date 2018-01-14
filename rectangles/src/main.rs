b0a8732ec0966c8e8f493b7597fcde32cedf6c90
Ryan Flach <ryanflach@users.noreply.github.com>
2018-01-14 09:09:07 -0700

    Associated functions

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -16,12 +16,18 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+
+    // Associated functions do not take `self` as a parameter
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
     let rect2 = Rectangle { width: 10, height: 40 };
-    let rect3 = Rectangle { width: 60, height: 45 };
+    // Associated functions are called with :: syntax
+    let rect3 = Rectangle::square(40);
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
