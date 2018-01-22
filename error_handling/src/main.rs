b6277630f3a4b1534b9bcfd4504abd8a2785e83d
Ryan Flach <ryanflach@users.noreply.github.com>
2018-01-21 17:56:26 -0700

    Handling multiple types of errors with match guard

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,12 +1,29 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
     let f = File::open("hello.txt");
 
     let f = match f {
         Ok(file) => file,
+        // In the context of a pattern, & matches a reference and gives us its
+        // value, but ref matches a value and gives us a reference to it.
+        Err(ref error) if error.kind() == ErrorKind::NotFound => {
+            match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => {
+                    panic!(
+                        "Tried to create file but there was a problem: {:?}",
+                        e
+                    )
+                },
+            }
+        },
         Err(error) => {
-            panic!("There was a problem opening the file: {:?}", error)
+            panic!(
+                "There was a problem opening the file: {:?}",
+                error
+            )
         },
     };
 }
