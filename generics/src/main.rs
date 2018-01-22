73c37e2d9eade806121594425b887dfa73b51851
Ryan Flach <ryanflach@users.noreply.github.com>
2018-01-22 16:17:40 -0700

    Using generics with method definitions of multiple types

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,22 +1,31 @@
-struct Point<T> {
+struct Point<T, U> {
     x: T,
-    y: T,
+    y: U,
 }
 
-// `<T>` after `impl` is required so Rust knows that these methods are available
-// on all instances, not just concrete types.
-impl<T> Point<T> {
-    fn x(&self) -> &T {
-        &self.x
-    }
-
-    fn y(&self) -> &T {
-        &self.y
+// Take two Point instances with up to 4 different total types and return a new
+// Point instance that is of types P1.x, P2.y.
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
     }
 }
 
 fn main() {
-    let p = Point { x: 5, y: 10 };
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c'};
 
-    println!("p.x = {}, p.y = {}", p.x(), p.y());
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
+
+// Note on performance with generics:
+// There is no runtime difference in performance when using generics vs concrete
+// types, thanks to Rust's use of monomorphization (turning generic code into
+// specific code at compile time). "We can write the non-duplicated code using
+// generics, and Rust will compile that into code that specifies the type in
+// each instance."
