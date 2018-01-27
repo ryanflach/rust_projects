9ee78afeb6449077f4b4432684c98cc28dbcdf47
Ryan Flach <ryanflach@users.noreply.github.com>
2018-01-27 10:58:58 -0700

    Using assert_eq

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,32 +1,13 @@
-#[derive(Debug)]
-pub struct Rectangle {
-    length: u32,
-    width: u32,
-}
-
-impl Rectangle {
-    pub fn can_hold(&self, other: &Rectangle) -> bool {
-        self.length > other.length && self.width > other.width
-    }
+pub fn add_two(a: i32) -> i32 {
+    a + 2
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*; // make anything in the outer module available in tests
+    use super::*;
 
     #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectangle { length: 8, width: 7 };
-        let smaller = Rectangle { length: 5, width: 1 };
-
-        assert!(larger.can_hold(&smaller));
-    }
-
-    #[test]
-    fn smaller_cannot_hold_larger() {
-        let larger = Rectangle { length: 8, width: 7 };
-        let smaller = Rectangle { length: 5, width: 1 };
-
-        assert!(!smaller.can_hold(&larger));
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
     }
 }
