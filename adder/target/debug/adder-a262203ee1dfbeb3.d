1d0a011eff34591d35d3a639f0256a35f45e8acd
Ryan Flach <ryanflach@users.noreply.github.com>
2018-01-27 10:47:49 -0700

    Add example code and tests using assert

---
+++ adder/target/debug/adder-a262203ee1dfbeb3.d
@@ -0,0 +1,1 @@
+/Users/ryanflach/Code/rust_projects/adder/target/debug/adder-a262203ee1dfbeb3: /Users/ryanflach/Code/rust_projects/adder/src/lib.rs
---
+++ adder/target/debug/libadder.d
@@ -0,0 +1,1 @@
+/Users/ryanflach/Code/rust_projects/adder/target/debug/libadder.rlib: /Users/ryanflach/Code/rust_projects/adder/src/lib.rs
