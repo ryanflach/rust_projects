2143895f151983b031bf67648faaabc9fca547be
Ryan Flach <ryanflach@users.noreply.github.com>
2018-01-28 11:20:33 -0700

    Use option expected param with #[should_panic]
    - Make test more precise by checking that the failure message contains the
    text provided by the expected parameter

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,11 +1,16 @@
+
 pub struct Guess {
     value: u32,
 }
 
 impl Guess {
     pub fn new(value: u32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100, got {}.", value);
+        if value < 1 {
+            panic!("Guess value must be greater than or equal to 1, got {}.",
+                   value);
+        } else if value > 100 {
+            panic!("Guess value must be less than or equal to 100, got {}.",
+                   value);
         }
 
         Guess {
@@ -19,7 +24,7 @@ mod tests {
     use super::*;
 
     #[test]
-    #[should_panic]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
     fn greater_than_100() {
         Guess::new(200);
     }
