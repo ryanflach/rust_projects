4e7b107d3cbe11c64fdcf9b4748177d95c2f6348
Ryan Flach <ryanflach@users.noreply.github.com>
2018-02-06 10:51:41 -0800

    Add sample file and basic file reading logic

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     // Using args() instead of args_os() to use String instead of OsString (more complex)
@@ -11,4 +13,13 @@ fn main() {
     // TODO: Error handling for invalid number of arguments
     println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    let mut f = File::open(filename).expect("file not found");
+
+    // Temporary: Just testing that the file is read properly.
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
