046937c3648bb343a668ba6ff590ec352fc240f5
Ryan Flach <ryanflach@users.noreply.github.com>
2018-02-07 08:36:08 -0800

    Add ENV variable and case-insensitive search functionality

--- minigrep/target/debug/minigrep-fe5a374423de1f20.d
+++ minigrep/target/debug/minigrep-fe5a374423de1f20.d
@@ -1,1 +1,1 @@
-/Users/ryanflach/rust_projects/minigrep/target/debug/minigrep-fe5a374423de1f20.dSYM: /Users/ryanflach/rust_projects/minigrep/src/main.rs /Users/ryanflach/rust_projects/minigrep/src/lib.rs
+/Users/ryanflach/rust_projects/minigrep/target/debug/minigrep-fe5a374423de1f20.dSYM: /Users/ryanflach/rust_projects/minigrep/src/lib.rs /Users/ryanflach/rust_projects/minigrep/src/main.rs
